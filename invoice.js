7d2d441cf0bfdcce94e3c8b3d7a3de6e7b178139
Dare2python <dare2python@gmail.com>
2018-08-06 17:03:01 +0300

    Pretty logging

--- invoice.js
+++ invoice.js
@@ -1,1 +1,46 @@
-console.log('JS is working');
+const util = require('util');
+
+function createInvoice( invoiceNumber ){
+    return {
+        invoiceNumber,
+        items: []
+    };
+};
+
+// console.log(createInvoice(1)  );
+
+function clone( o ){
+    return JSON.parse(  JSON.stringify( o ) );
+}
+
+// console.log(createInvoice(1), clone(createInvoice(1)) );
+
+function addItem(
+    invoice,
+    quantity,
+    itemPrice,
+    description
+){
+    const invoiceItems = clone( invoice.items );
+    const newItem = {
+        quantity,
+        itemPrice,
+        description
+    };
+    return {
+        invoiceNumber: invoice.invoiceNumber,
+        items: [ ...invoiceItems, newItem ]
+    };
+};
+
+i = addItem( createInvoice(1), 2, 10, 'T-Shirt');
+console.log( i );
+console.dir( i, { depth: null })
+console.log(util.inspect(i, { compact: true, showHidden: false, depth: null }));
+console.log(util.inspect(i, { colors: true, compact: false, showHidden: false, depth: null }));
+
+function f( o ){
+    console.log(util.inspect(o, { colors: true, compact: false, showHidden: false, depth: null }));
+};
+
+f( i );
