8b66be6667d83de0ad6fe8436b83a37d4f970f94
Dare2python <dare2python@gmail.com>
2018-08-06 17:42:06 +0300

    Log Reducer

--- invoice.js
+++ invoice.js
@@ -1,28 +1,41 @@
 // import { f } from './l';
 const l = require('./l');
 
-function createInvoice( invoiceNumber ){
+function createInvoice(invoiceNumber) {
     return {
         invoiceNumber,
         items: []
     };
 };
 
-function clone( o ){
-    return JSON.parse(  JSON.stringify( o ) );
+function clone(o) {
+    return JSON.parse(JSON.stringify(o));
 }
 
-function addItem( invoice, quantity, itemPrice, description ) {
-    const newInvoice = clone( invoice );
-    l.f( newInvoice );
-    newInvoice.items.push( {
+function addItem(invoice, quantity, itemPrice, description) {
+    const newInvoice = clone(invoice);
+    l.f(newInvoice);
+    newInvoice.items.push({
         quantity,
         itemPrice,
         description
-    } );
+    });
     return newInvoice;
 };
 
-i = addItem( createInvoice(1), 2, 10, 'T-Shirt');
+i = addItem(createInvoice(1), 2, 10, 'T-Shirt');
+i = addItem(i, 3, 25, 'Bag');
 
-l.f( i );
+l.f(i);
+
+function calculateSum(invoice) {
+    const logReducer = (acc, item) => {
+        l.f(acc);
+        l.f(item);
+        return acc + item.quantity * item.itemPrice;
+    }
+
+    return invoice.items.reduce(logReducer, 0);
+};
+
+l.f( calculateSum( i ) );
