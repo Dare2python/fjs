d28f3a73ea7e2d84b0eefcb39e4230338fa2c1fd
Dare2python <dare2python@gmail.com>
2018-08-07 13:13:27 +0300

    General sequncing gunction

--- l.js
+++ l.js
@@ -8,13 +8,28 @@ function f( o ){
     console.log(util.inspect(o, { colors: true, compact: true, showHidden: true, depth: null, showProxy: true }));
 };
 
-sequence = ( f1, f2 ) =>
+sequence2 = ( f1, f2 ) =>
     (...args) =>
     f2( f1( ...args ) );
 
+// sequence = ( head, ...tail) =>
+//     (...args) => {
+//         tail.reduce( sequence2, head(...args))
+//     };
+
+// sequence = ( ...ff) =>
+//     (...args) => {
+//         ff.reduce( sequence2, ...args );
+//     };
+
+sequence = ( head, ...tail) =>
+    tail.reduce( sequence2, head );
+
+
 const f1 = ( a, b ) => a + b;
-const f2 = a => `Result: ${a}`;
+const f2 = a => `f2${a}`;
+const f3 = a => `f3${a}`;
 
-console.log( f2( f1(  2, 5 ) ) );
+console.log( f3( f2( f1(  2, 5 ) ) ) );
 
-console.log( sequence( f1, f2 )(2, 5) );
+console.log( sequence( f1, f2, f3 )(2, 5) );
