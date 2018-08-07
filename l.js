6e74a05937f5342face18d42e3a1395a48480639
Dare2python <dare2python@gmail.com>
2018-08-07 12:07:04 +0300

    Shallow cloning

--- l.js
+++ l.js
@@ -7,3 +7,12 @@ module.exports = {
 function f( o ){
     console.log(util.inspect(o, { colors: true, compact: true, showHidden: true, depth: null, showProxy: true }));
 };
+
+const cloneArray = arr => [...arr];
+
+const originalArray = ['hi', {on:true}];
+
+const newArray = cloneArray( originalArray );
+
+f(originalArray );
+f(newArray );
