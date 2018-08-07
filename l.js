4dc226512b87538fe573e9db3ef51f2dd7b68ad7
Dare2python <dare2python@gmail.com>
2018-08-07 13:45:33 +0300

    head and tail

--- l.js
+++ l.js
@@ -8,28 +8,9 @@ function f( o ){
     console.log(util.inspect(o, { colors: true, compact: true, showHidden: true, depth: null, showProxy: true }));
 };
 
-sequence2 = ( f1, f2 ) =>
-    (...args) =>
-    f2( f1( ...args ) );
+const arr = [1, 2, 3, 4];
 
-// sequence = ( head, ...tail) =>
-//     (...args) => {
-//         tail.reduce( sequence2, head(...args))
-//     };
+const [head, ...tail] = arr;
 
-// sequence = ( ...ff) =>
-//     (...args) => {
-//         ff.reduce( sequence2, ...args );
-//     };
-
-sequence = ( head, ...tail) =>
-    tail.reduce( sequence2, head );
-
-
-const f1 = ( a, b ) => a + b;
-const f2 = a => `f2${a}`;
-const f3 = a => `f3${a}`;
-
-console.log( f3( f2( f1(  2, 5 ) ) ) );
-
-console.log( sequence( f1, f2, f3 )(2, 5) );
+f(head);
+f(tail);
